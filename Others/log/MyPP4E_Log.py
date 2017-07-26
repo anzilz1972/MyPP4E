6daade31a4fb3fee22ead1122ae5009b9b4a39e6
anzilz1972 <18909311025@189.cn>
2017-07-26 14:15:01 +0800

    2017.07.26 ÍêÉÆloggerº¯Êý£¬¼ÓÈë»عöÎļþlog

--- Others/log/MyPP4E_Log.py
+++ Others/log/MyPP4E_Log.py
@@ -5,9 +5,9 @@
 # @email   : 18909311025@189.cn 
 # @Version : 1.0
 """
-  摘自：python标准模块--logging http://www.cnblogs.com/zhbzz2007/p/5943685.html
-  1、测试使用logging模块，使用logging.Logger类
-  2、loglevel
+  1、摘自：python标准模块--logging http://www.cnblogs.com/zhbzz2007/p/5943685.html
+  2、测试使用logging模块，使用logging.Logger类
+  3、loglevel
     NOTSET = 0
     DEBUG = 10
     INFO = 20
@@ -15,37 +15,49 @@
     ERROR = 40
     CRITICAL = FATAL = 50
 """
-import logging
+import logging,time
+from logging.handlers import RotatingFileHandler
 
-def mypp4e_log():
+
+def mypp4e_log(rotateLog = True, consoleLog = True):
 	logger = logging.Logger(name = 'MyPP4Elog')
 	logger.setLevel(logging.DEBUG)
 
+	fileFormatter = logging.Formatter('{asctime}|{name}|{pathname}|{levelno}|{levelname}|{message}',style = '{')
+	if rotateLog:
+		#使用RotatingFileHandler,可以实现日志回滚
+		#创建RotatingFileHandler,设置log级别为WARNING，设置log格式其风格为'{'
+		#每个log文件大小为1M,上限100个文件
+		rHandler = RotatingFileHandler('rlog.txt', maxBytes = 1024*1024, backupCount = 100)
+		rHandler.setLevel(logging.WARNING)
+		rHandler.setFormatter(fileFormatter)
+		logger.addHandler(rHandler)					#正常log文件log.txt
+	else:
+		#创建Filehandler,设置log级别为WARNING,设置log格式其风格为'{'
+		handler = logging.FileHandler('log.txt')      
+		handler.setLevel(logging.WARNING)                
+		handler.setFormatter(fileFormatter)
+		logger.addHandler(handler)	                #可回滚log文件rlog.txt
 
-	#创建Filehandler,设置log级别为INFO,设置log格式其风格为'{'
-	handler = logging.FileHandler('e:\ProgrammingPython\MyPP4E\Mypp4eLog.txt')      
-	handler.setLevel(logging.INFO)                
-	formatter1 = logging.Formatter('{asctime}|{name}|{pathname}|{levelno}|{levelname}|{message}',style = '{')	          
-	handler.setFormatter(formatter1)
-
-	#log同时也输出到stderr，设置log级别为DEBUG,设置log格式其风格为'{'
-	console = logging.StreamHandler()
-	console.setLevel(logging.DEBUG)
-	formatter2 = logging.Formatter('{asctime}|{name}|{levelno}|{levelname}|{message}',style = '{')
-	console.setFormatter(formatter2)	
-
-	#将filehandler和console都添加到当前logger对象
-	logger.addHandler(handler)
-	logger.addHandler(console)
+	#log也可以输出到stderr，设置log级别为DEBUG,设置log格式其风格为'{'
+	if consoleLog:
+		console = logging.StreamHandler()
+		console.setLevel(logging.DEBUG)
+		consoleformatter = logging.Formatter('{asctime}|{name}|{levelno}|{levelname}|{message}',style = '{')
+		console.setFormatter(consoleformatter)	
+		logger.addHandler(console)
+	
 	return logger
 
 if __name__ == '__main__':
 	logger = mypp4e_log()
-	logger.debug('Houston, we have a %s', 'thorny problem')
-	logger.info('Houston, we have a %s', 'interesting problem')
-	logger.error('Houston, we have a %s', 'major problem')
-	logger.warning('Houston, we have a %s', 'bit of a problem')
-	logger.critical('Houston, we have a %s', 'major disaster')
+	for i in range(100000):
+		logger.debug('Houston, we have a %s', 'thorny problem')
+		logger.info('Houston, we have a %s', 'interesting problem')
+		logger.warning('Houston, we have a %s', 'bit of a problem')
+		logger.error('Houston, we have a %s', 'major problem')
+		logger.critical('Houston, we have a %s', 'major disaster')
+		time.sleep(2)
 
 
 
